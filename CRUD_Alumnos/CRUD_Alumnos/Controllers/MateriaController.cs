1d82f4b68fdaf14a49b488d7bfe9383f1c9ff768
maritza1 <maritzafunes098@outlook.es>
2021-02-18 23:06:16 -0600

    Creation of CRUD for Materia

---
+++ CRUD_Alumnos/CRUD_Alumnos/Controllers/MateriaController.cs
@@ -0,0 +1,125 @@
+﻿using CRUD_Alumnos.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CRUD_Alumnos.Controllers
+{
+    public class MateriaController : Controller
+    {
+        // GET: Materia
+        public ActionResult Index()
+        {
+            AlumnosContext db = new AlumnosContext();
+            return View(db.Materia.ToList());
+        }
+
+        //Agregar materia
+        public ActionResult Agregarmateria()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Agregarmateria(Materia m)
+        {
+            if (!ModelState.IsValid)
+                return View();
+            try
+            {
+                using (var db = new AlumnosContext())
+                {
+                    db.Materia.Add(m);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "Error - " + ex.Message);
+                return View();
+            }
+        }
+        //Listar los docentes para las secciones Agregar y Editar
+        public ActionResult ListaDocentes()
+        {
+            using (var db = new AlumnosContext())
+            {
+                return PartialView(db.Docente.ToList());
+            }
+        }
+        //Editar de la materia
+        public ActionResult Editarmateria(int id)
+        {
+            try
+            {
+                using (var db = new AlumnosContext())
+                {
+                    Materia mat = db.Materia.Find(id);
+                    return View(mat);
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "Error - " + ex.Message);
+                return View();
+            }
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Editarmateria(Materia m)
+        {
+            if (!ModelState.IsValid)
+                return View();
+            try
+            {
+                using (var db = new AlumnosContext())
+                {
+                    Materia mat = db.Materia.Find(m.Id_Materia);
+                    mat.Nombre = m.Nombre;
+                    mat.id_docente = m.id_docente;
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "Error - " + ex.Message);
+                return View();
+            }
+        }
+
+        //Detalles de la materia
+        public ActionResult Detallesmateria(int id)
+        {
+            using (var db= new AlumnosContext())
+            {
+                Materia mat = db.Materia.Find(id);
+                return View(mat);
+            }
+        }
+        //Mostar el nombre del docente en los detalles de la materia
+        public static string NombreDocente(int id_docente)
+        {
+            using (var db = new AlumnosContext())
+            {
+                return db.Docente.Find(id_docente).NombreCompleto;
+            }
+        }
+
+        //Eliminar una materia
+        public ActionResult Eliminarmateria(int id)
+        {
+            using (var db = new AlumnosContext())
+            {
+                Materia mat = db.Materia.Find(id);
+                db.Materia.Remove(mat);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+        }
+    }
+}
---
+++ CRUD_Alumnos/CRUD_Alumnos/Models/MateriaCE.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CRUD_Alumnos.Models
+{
+    public class MateriaCE
+    {
+        [Required]
+        public string Nombre { get; set; }
+        [Required]
+        [Display(Name = "Docente")]
+        public int id_docente { get; set; }
+    }
+    [MetadataType(typeof(MateriaCE))]
+    public partial class Materia
+    {
+
+    }
+
+}
