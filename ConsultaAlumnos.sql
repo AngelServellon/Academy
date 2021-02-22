6d2169981064e254f75a52a622ab628c5a65de70
maritza1 <maritzafunes098@outlook.es>
2021-02-21 22:34:24 -0600

    Adding styles to the applicaion

--- ConsultaAlumnos.sql
+++ ConsultaAlumnos.sql
@@ -3,7 +3,7 @@ from Alumno a
 inner join Ciudad c on a.CodCiudad = c.id
 where a.Edad > 18;
 
-select distinct a.Nombre, a.Apellidos, m.Nombre as Materia from Alumno a 
+select distinct a.Nombre, a.Apellidos, m.Nombre as Materia, c.Nota1, c.Nota2, c.Nota3, c.Nota4 from Alumno a 
 inner join Calificacion c 
 on a.id = c.id_alumno
 inner join Materia m 
